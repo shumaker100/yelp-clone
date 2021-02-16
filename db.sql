e17a923911626a5d1d094e3ed3c495a32fe53398
Jason Shumaker <utahshu@gmail.com>
2021-02-16 14:13:44 -0700

    node install

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE restaurants (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR (50) NOT NULL,
+    price_range INT NOT NULL check(price_range >=1 and price_range <= 5)
+);
+
+INSERT INTO restaurants (id, name, location, price_range) values (123, 'mcdonalds', 
+'new york', 3);
