f80a06c0ee85e7f5ad7a1a1e3883d921579998ae
Jason Shumaker <utahshu@gmail.com>
2021-02-22 11:07:28 -0700

    update

---
+++ client/src/components/RestaurantList.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import Restaurantfinder from "../apis/Restaurantfinder";
+
+const RestaurantList = () => {
+  useEffect(async () => {
+    try {
+      const response = await Restaurantfinder.get("/");
+      console.log(response);
+    } catch (err) {}
+  }, []);
+
+  return (
+    <div className="list-group">
+      <table className="table table-hover table-dark">
+        <thead>
+          <tr className="bg-primary">
+            <th scope="col">Restaurant</th>
+            <th scope="col">Location</th>
+            <th scope="col">Price Range</th>
+            <th scope="col">Rating</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>McDonalds</td>
+            <td>New York</td>
+            <td>$$</td>
+            <td>Rating</td>
+            <td>
+              <button className="btn btn-warning">Update</button>
+            </td>
+            <td>
+              <button className="btn btn-danger">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default RestaurantList;
---
+++ client/src/routes/UpdatePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdatePage = () => {
+  return <div>updatepage</div>;
+};
+
+export default UpdatePage;
